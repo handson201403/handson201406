8f83a6d971a17f1f48bd2b1b416a15bc38be6370
yutaro <yutaro@prime-strategy.co.jp>
2014-06-11 22:20:59 +0900

    add menu

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -94,7 +94,10 @@ function twentythirteen_setup() {
 	) );
 
 	// This theme uses wp_nav_menu() in one location.
-	register_nav_menu( 'primary', __( 'Navigation Menu', 'twentythirteen' ) );
+	register_nav_menus ( array (
+	  'primary' => __( 'Navigation Menu', 'twentythirteen' ), 
+	  'secondary' => __( 'Sub Menu', 'twentythirteen' )
+	));
 	
 	/*
 	 * This theme uses a custom image size for featured images, displayed on
--- wp-content/themes/twentythirteen/sidebar.php
+++ wp-content/themes/twentythirteen/sidebar.php
@@ -15,7 +15,7 @@ if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 	<div id="tertiary" class="sidebar-container" role="complementary">
 		<div class="sidebar-inner">
 			<div class="widget-area">
-				
+				<?php wp_nav_menu( array('menu' => 'Sub Menu', 'container' => 'ul')); ?>
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div><!-- .widget-area -->
 		</div><!-- .sidebar-inner -->
