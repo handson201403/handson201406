afae9f6a9fa2fe3745f9cfce236fca17e4475dc1
yutaro <yutaro@prime-strategy.co.jp>
2014-06-11 14:40:48 +0900

    update functions

--- wp-content/themes/night-in-jakarta/functions.php
+++ wp-content/themes/night-in-jakarta/functions.php
@@ -24,10 +24,4 @@ function night_in_jakarta_setup() {
 				)
 			);
 }
-add_editor_style('editor-style.css');
-		function custom_editor_settings( $initArray ) {
-		    $initArray['body_class'] = 'editor-area';
-		    return $initArray;
-		}
-		add_filter( 'tiny_mce_before_init', 'custom_editor_settings' );
 ?>
