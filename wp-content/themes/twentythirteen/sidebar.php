4ec7a20c32d8cb1bfd06f03767d3e418c283e2b5
yutaro <yutaro@prime-strategy.co.jp>
2015-08-26 18:39:25 +0900

    wordpress update

--- wp-content/themes/twentythirteen/sidebar.php
+++ wp-content/themes/twentythirteen/sidebar.php
@@ -15,7 +15,6 @@ if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 	<div id="tertiary" class="sidebar-container" role="complementary">
 		<div class="sidebar-inner">
 			<div class="widget-area">
-				<?php wp_nav_menu( array('theme_location' => 'primary-menu', 'container' => '')); ?>
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div><!-- .widget-area -->
 		</div><!-- .sidebar-inner -->
